88c7a90ad074cf4d0fa0f450d4b9f14d4b7cfc21
Rachealelias <rachealsamuel708@gmail.com>
2021-09-11 17:18:30 -0400

    commit tree cb8f7f767aba4ed19615cc6a55c8829c160c838c

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,4 +1,5 @@
 import React, {useState,useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 
 function QuestionList() {
@@ -12,21 +13,51 @@ function QuestionList() {
       });
   },[]);
    const displayList = questions.map((question)=>(
- <li key ={question.id}>
-     prompt: {question.prompt} 
-     answers:{question.answers}
-     correctindex:{question.correctIndex}
-    </li>
+//  <li key ={question.id}>
+//      prompt: {question.prompt} 
+//      answers:{question.answers}
+//      correctindex:{question.correctIndex}
+//     </li>
+   <QuestionItem question = {question}
+      deleteButton= {deleteButton}
+      patchAnswers= {patchAnswers}
+         />
 
    ))
-   const deleteQuestion = questions.map((question)=>(
-     <button>{question}</button>
-   ))
+   function deleteButton(id){
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "DELETE"
+    })
+    .then(res => res.json())
+    .then(()=>{
+      const updateQuestion = questions.filter(ques => ques.id !== id)
+      setQuestions(updateQuestion)
+    })
+   }
+
+   function patchAnswers(id, correctIndex){
+     fetch(`http://localhost:4000/questions/${id}`,{
+       method:"PATCH",
+       headers:
+       { "Content-Type": "application/json" },
+       body: JSON.stringify({correctIndex})
+     })
+     .then(res => res.json())
+     .then(data=>{
+       const updatedQuestion = questions.map(ques =>{
+         if (ques.id === data.id){
+           return data
+         }
+         })
+     setQuestions(updatedQuestion)
+    })
+  }
+   
   return (
     <section>
       <h1>Quiz Questions</h1>
       <ul>{displayList}</ul>
-      {deleteQuestion}
+
     </section>
   );
 }
